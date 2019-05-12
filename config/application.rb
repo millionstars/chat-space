6268679b4b391207ecddc5c87a27a28ed302f7be
millionstars <flashback169@yahoo.co.jp>
2019-05-12 15:14:36 +0900

    Add config application.rb for not generate coffee file

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,11 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
+      
+      config.generators do |g|
+        g.javascripts false
+        g.helper false
+        g.test_framework false
     end
   end
 end
