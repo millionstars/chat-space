5558fcb99a349aab9ca02c1497ca84e80d91ecd2
millionstars <flashback169@yahoo.co.jp>
2019-05-25 19:28:03 +0900

    Update message_controller create set_group

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,20 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
-    
+
   end
+
+  def create
+
+  end
+  private
+  def message_params
+
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+  
+  
 end
