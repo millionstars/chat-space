c151fea6ad61fead30f64f2f96bdd7c54e8e2954
millionstars <flashback169@yahoo.co.jp>
2019-05-26 16:07:48 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,14 @@ class MessagesController < ApplicationController
   end
 
   def create
-
+    @messages = @group.messages.new(message_params)
+    if message.save
+      redirect_to group_messages_path(@group). notice: 'メッセージが送信されました。'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力して下さい。'
+      render :index
+    end
   end
   private
   def message_params
