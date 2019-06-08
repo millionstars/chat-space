327fbfc20547ccddedb55ad735d6b84977809405
millionstars <flashback169@yahoo.co.jp>
2019-06-08 14:02:46 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,9 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
+      respond_to do |format|
+        format.html
+        format.json
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました。'
     else
       @messages = @group.messages.includes(:user)
