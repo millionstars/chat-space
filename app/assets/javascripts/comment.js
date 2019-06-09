e29991081e4443a6e5d7eaf46c9c2fe5094f2624
millionstars <flashback169@yahoo.co.jp>
2019-06-09 20:33:59 +0900

    Update comment.js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,6 +1,29 @@
 $(function() {
-  $(`#new_message`).on(`submit`, function(e){
+  function buildHTML(message){
+    var img = message.image? `<img src=${message.image}>` : "";
+
+    var html = `<div class="message" >
+                  <div class="upper-message">
+                    <p class="upper-message__user-name">
+                    ${message.user_name}
+                    </p>
+                    <p class="upper-message__date">
+                    ${message.date}
+                    </p>
+                    </div>
+                  <div class="lower-message">
+                    <div class="lower-message__content">
+                    ${message.content}
+                  </div>
+                  <div class="lower-message__content">
+                    ${img}
+                  </div>
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
+    console.log(this);
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -11,5 +34,15 @@ $(function() {
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      $('#new_message')[0].reset();
+     })
+    .fail(function(data){
+      alert('error');
+    })
+    return false;
   })
-})
+});
