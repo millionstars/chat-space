a5d99cd9ce9a4ad904f66f7d4ac7bc4bd1a16dbc
millionstars <flashback169@yahoo.co.jp>
2019-06-15 14:24:21 +0900

    エラー時の表示をconsoleでなくalertに修正

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -64,7 +64,7 @@ $(function() {
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function(){
-      console.log('error');
+      alert('error');
     })
   };
   setInterval(reloadMessages, 5000);
